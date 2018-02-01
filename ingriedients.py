5b54e6cbe040593d67f9afa7190979a0f602626e
MeganC123 <16MCollins@stjosephsrush.ie>
2018-02-01 09:40:19 +0000

    Update ingriedients.py

--- ingriedients.py
+++ ingriedients.py
@@ -1,38 +1,70 @@
+def yes():
+                print("ok then ")
+                print("what would you like to make?")
+                print("if you would like more options type no ")
+                choice=input("you can make a.sponge cake b.toasted sandwhich c.omelette d. sandwhich     e. scrambled eggs")
+                if choice=="a":
+                    print("ok the ingriedents are")
+                    print( "butter")
+                    print("milk")
+                    print("eggs")
+                    print("flower(not in fridge)")
+                    print("baking powder(not in fridge)")
+                    print ("sugar(not in fridge)")
+                  
+
+
+                elif choice=="b":
+                    print("ok you need")
+                    print ("cheese")
+                    print("ham")
+                    print("butter")
+                    print("bread(not in fridge)")
+                    
+                elif choice=="c":
+                    print("alright you'll need")
+                    print("eggs")
+                    print("butter")
+                    print("milk OR water")
+                    print("black peeper(not kept in a fridge)")
+                    
+
+
+                elif choice=="d":
+                    print ("sure you can make a sandwhich")
+                    print (" all you need is ...")
+                    print("ham")
+                    print ("cheese")
+                    print("bread(not kept in a fridge)")
+
+
+
+
+                elif choice=="e":
+                    print("ok you'll need")
+                    print("eggs")
+                    print("milk")
+                    print("salt and pepper")
+
+
+
+                
+
+                
+                
+
+
+def no(): 
+        ("ok")
+        
+        
+
+
 while True:
     option=input("would you like to know the ingreidents to make something")
     if option=="yes":
-            print("ok then ")
-            print("what would you like to make?")
-            choice=input("a.sponge cake b.toasted sandwhich c.omlette")
-            if choice=="a":
-                print("ok the ingriedents are")
-                print( "butter")
-                print("milk")
-                print("eggs")
-                print("flower(not in fridge)")
-                print("baking powder(not in fridge)")
-                print ("sugar(not in fridge)")
-                break
+        yes()
+    elif option == "no":
+        no()
+        break
 
-
-            elif choice=="b":
-                print("ok you need")
-                print ("cheese")
-                print("ham")
-                print("butter")
-                print("bread(not in fridge)")
-                break
-
-            elif choice=="c":
-                print("alright you'll need")
-                print("eggs")
-                print("butter")
-                print("milk OR water")
-                print("black peeper(not kept in a fridge)")
-                break
-
-
-
-    elif option=="no":
-            ("ok")
-            break
